44aaf378c14d70474e6f6621bba48aca3827796e
adisak <adisak@gmail.com>
2022-12-14 19:48:40 -0600

    Update example
    Show string handling example

--- test/Eval_Test.bat
+++ test/Eval_Test.bat
@@ -1,6 +1,6 @@
 @echo off
 REM  "Multi" is a thread-pool emulation helper library for controlling multi-threaded windows batch [*.BAT] files
-REM  Copyright (c) 2020-2021 Adisak Pochanayon
+REM  Copyright (c) 2020-2022 Adisak Pochanayon
 REM  Contact: adisak@gmail.com
 REM  See EvalBat_License.txt for details
 REM  Currently hosted at https://github.com/adisak/EvalBat
@@ -19,6 +19,10 @@ call EvalBat_Echo.bat Abs(1 - 2)
 
 echo.
 call EvalBat_Echo.bat Int((100 / 3)*100)/100
+
+REM Now with simple string support, use two single quotes to emulate a double quote
+echo.
+call EvalBat_Echo.bat UCase(''hello'') + '' world''
 
 ENDLOCAL
 goto:EOF
